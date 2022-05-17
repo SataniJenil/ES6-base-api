6a437db1a5248de4c8d8ff1ac90418bcf85b7026
jenil satani <jenil.igenerate@gmail.com>
2022-05-17 13:03:09 +0530

    change env name

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,18 +1,14 @@
 import { Sequelize } from "sequelize";
 import { initialize } from "../model";
 require("dotenv").config();
-var HOST = process.env.HOST;
-console.log("secret", HOST);
-var PORT = process.env.PostGres_PORT;
-console.log("PORT", PORT);
-var post = process.env.DATABASE;
-console.log("post", post);
-var get = process.env.UN;
-console.log("get", get);
-var put = process.env.PASSWORD;
-console.log("put", put);
 
-const connection = new Sequelize(post, get, put, {
+var HOST = process.env.DB_HOST;
+var PORT = process.env.PostGres_PORT;
+var post = process.env.DB_DATABASE;
+var get = process.env.DB_UN;
+var psw = process.env.DB_PASSWORD;
+
+const connection = new Sequelize(post, get, psw, {
   host: HOST,
   port: PORT,
   dialect: "postgres",
